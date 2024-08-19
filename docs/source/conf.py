6fe9abff32b60a0a588c55a29bfbfd7f37c82d2c
Herminial <herminial@l909.info>
2024-08-19 10:18:18 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'VLC Media Player'
+copyright = '2024, Hermina'
+author = 'Hermina'
 
 release = '0.1'
 version = '0.1.0'
